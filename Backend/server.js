c21951859af2607bb6b169f44680eea40f206507
Faaiz Umer <26100088@lums.edu.pk>
2025-03-30 16:44:54 +0500

    Connect to database

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,14 @@
 // server.js
 
 import app from './app.js';
+import connectDB from './config/db.js';
+
 
 const PORT = process.env.PORT || 8000;
+connectDB(); // Connect to MongoDB
+
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
