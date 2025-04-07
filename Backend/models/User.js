1b05a444e614b088af110a4c3af309a6326ae9f7
Faaiz Umer <26100088@lums.edu.pk>
2025-04-07 19:48:10 +0500

    Mannequin Page

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,11 +1,12 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
+  username: { type: String, required: false, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   likedItems: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product' }],
-  cart: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product' }]
+  ownedProducts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product' }],
+  gender: { type: String, required: true },
 });
 
 export default mongoose.model('User', userSchema);
