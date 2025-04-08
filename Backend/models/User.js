5d7d11dcb1f01af9a1c7570646c87b68ce264f56
Faaiz Umer <26100088@lums.edu.pk>
2025-04-08 12:17:04 +0500

    Complete auth integration

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 const userSchema = new mongoose.Schema({
   username: { type: String},
   email: { type: String, required: true, unique: true },
+<<<<<<< HEAD
   password: { type: String},
   likedItems: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Clothes' }],
   ownedProducts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Clothes' }],
@@ -15,16 +16,38 @@ const userSchema = new mongoose.Schema({
   pantSize: {
     type: String,
     enum: ['s','m','l'],
+=======
+  password: { type: String },
+  likedItems: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product' }],
+  ownedProducts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product' }],
+  gender: { type: String},
+  shirtSize: {
+    type: String,
+    enum: ['small', 'medium', 'large'],
+  },
+  pantSize: {
+    type: String,
+    enum: ['small', 'medium', 'large'],
+
+  },
+  weightClass: {
+    type: String,
+    enum: ['light', 'medium', 'heavy'],
+>>>>>>> c24c37e (Complete auth integration)
 
   },
   stylePreference : {
     type: String,
+<<<<<<< HEAD
     enum: [
       'Modern',
       'Old_Money',
       'Smart_Casual',
       'Casual_Everyday'
     ],
+=======
+    enum: ['modern', 'business', 'casual', 'oldmoney'],
+>>>>>>> c24c37e (Complete auth integration)
   }
 });
 
