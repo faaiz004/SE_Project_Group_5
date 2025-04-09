592fad6fa4453574d5359851e4fa2636e39b0c06
Faaiz Umer <26100088@lums.edu.pk>
2025-04-09 21:46:54 +0500

    Fixed Problems

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 const userSchema = new mongoose.Schema({
   username: { type: String},
   email: { type: String, required: true, unique: true },
-  password: { type: String, required : true },
+  password: { type: String},
   likedItems: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Clothes' }],
   ownedProducts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Clothes' }],
   gender: { type: String},
